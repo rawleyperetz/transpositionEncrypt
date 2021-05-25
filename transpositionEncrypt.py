cfe4d4251f7cb536bfaeca5ccceb7bd6104ba49c
rawleyperetz <70651978+rawleyperetz@users.noreply.github.com>
2021-05-25 00:05:14 +0000

    Update transpositionEncrypt.py

--- transpositionEncrypt.py
+++ transpositionEncrypt.py
@@ -5,19 +5,20 @@ import pyperclip
 def main():
     myMessage = 'Common sense is not so common.'
     myKey = 8
-    ciphertext = encryptMessage(myMessage, myKey)
+    cipherText = encryptMessage(myMessage, myKey)
     # Print the encrypted string in ciphertext to the screen, with
     # a | ("pipe" character) after it in case there are spaces at
     # the end of the encrypted message:
-    print(ciphertext + '|')
+    print(cipherText + '|')
     # Copy the encrypted string in ciphertext to the clipboard:
-    pyperclip.copy(ciphertext)
+    pyperclip.copy(cipherText)
 
 def encryptMessage(message:str,key:int)->str:
     #this is the code for transposition cipher
     message = list(message)
     rows = int(len(message) % key)
     spaces = [' ' for i in range(((rows*key) - len(message)) % key) if (rows*key) != len(message)]
+    #code lines 22 to 24 is the same as code line 20. 
     '''if (rows*key) != len(message):
         for i in range(((rows*key) - len(message)) % key):
             message.append(' ')'''
